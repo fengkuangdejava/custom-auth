7af770112fe5f0b3c3cd24af959057d1b0fb3a83
liting <fengkuangdejava@outlook.com>
2019-09-16 02:10:31 +0800

    11

--- src/main/java/com/example/auth/service/imp/DefaultAuthenticateAction.java
+++ src/main/java/com/example/auth/service/imp/DefaultAuthenticateAction.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class DefaultAuthenticateAction implements IAuthenticateAction {
     @Override
     public void authenticate(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain){
-        //从参数中获取token 去redis取 没取到 报异常 取到了 接着走
+        //从参数中获取token 去redis取(tokenService) 没取到 报异常 取到了 接着走
         //取到后根据clientID 查一遍数据库
         //匹一下过期时间 和权限范围  不匹配，报错 客户端凭证过期或者 客户端权限不够
         //然后根据用户角色 匹一下 requestUrl;查看是否匹配  不匹配 报用户没有权限 匹配 这dofilter;
